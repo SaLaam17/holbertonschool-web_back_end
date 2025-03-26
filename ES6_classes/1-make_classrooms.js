2fa447d06e2702ff6b5b3e8811f99830a76f816a
SaLaam17 <slaamri@yahoo.fr>
2025-03-26 12:45:04 +0100

    1-make_classrooms.js

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,4 +1,9 @@
-import ClassRoom from "./0-classroom.js";
-function initializeRooms (){
-  return an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order).
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+
+  return [room1, room2, room3];
 }
