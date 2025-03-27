5149be0a22a25f548f73650fc02ea035ec83f768
SaLaam17 <slaamri@yahoo.fr>
2025-03-27 14:31:00 +0100

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+    if (this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
@@ -9,26 +9,5 @@ export default class Building {
   get sqft() {
     return this._sqft;
   }
+// this.constructor !== Building &&
 }
-/*
-  constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly');
-    }
-
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-
-    this._sqft = sqft;
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-*/
