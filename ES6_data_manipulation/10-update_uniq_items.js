a485d6f2207bdd086db51cd7c5621a727bc4ed3d
SaLaam17 <slaamri@yahoo.fr>
2025-04-01 21:32:48 +0200

    10-update_uniq_items.js

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(map) {
+  console.log("updateUniqueItems() a été appelée !");
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+  return map;
+}
