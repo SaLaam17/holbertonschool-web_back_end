0c8928d195c159997d433f14b3659e64ccf71f07
SaLaam17 <slaamri@yahoo.fr>
2025-04-02 15:42:15 +0200

    2-then.js and 3-all.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-  const myPromise = new Promise((resolve, reject) => {
+  const myPromise = new Promise((resolve) => {
     resolve('Success');
   });
   return myPromise;
