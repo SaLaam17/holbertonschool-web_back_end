26641cc7c2d3d69e951b8f36bdc1f93723f686e6
SaLaam17 <slaamri@yahoo.fr>
2025-04-02 18:27:51 +0200

    fixing 3-all.js error one return missing

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  try {
-    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(photo.body, user.firstName, user.lastName);
-  } catch {
-    console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      return console.log(photo.body, user.firstName, user.lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+    
 }
