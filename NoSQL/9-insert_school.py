d0d0e1c6686910bafd96d4f26264f256e3963ae3
SaLaam17 <slaamri@yahoo.fr>
2025-05-01 13:55:56 +0200

    9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+Module for the "insert_school" function
+that inserts a new document
+in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    function that inserts a new document
+    in a collection based on kwargs
+    """
+    new_document = mongo_collection.insert_one(kwargs)
+    return new_document.inserted_id
