4302b4f3258a9284f21e5775e508b6f3369432d0
SaLaam17 <slaamri@yahoo.fr>
2025-05-19 12:42:28 +0200

    fixing eslint errors inside 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,25 @@
 const readline = require('node:readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-console.log(`Welcome to Holberton School, what is your name?`);
+  console.log('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
-});
+  rl.on('line', (input) => {
+    console.log(`Your name is: ${input}`);
+    rl.close();
+  });
 
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});
+  rl.on('close', () => {
+    console.log('This important software is now closing');
+  });
+}
+
+module.exports = main;
+
+if (require.main === module) {
+  main();
+}
