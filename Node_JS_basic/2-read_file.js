f681ab954f701a86acf4c6b9e6114cce578bfe12
SaLaam17 <slaamri@yahoo.fr>
2025-05-20 09:59:53 +0200

    2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,9 +1,10 @@
+
 const fs = require('fs');
 
 function countStudents(path) {
-  let data
+  let data;
   try {
-    data = fs.readFileSync(path, 'utf8');
+    data = fs.readFileSync(path, 'utf-8');
   } catch (err) {
     throw new Error('Cannot load the database');
   }
@@ -12,7 +13,28 @@ function countStudents(path) {
 
   const students = lines.slice(1);
 
-  console.log(`Number of students: ${students.length}`);
+  const fields = {};
+
+  students.forEach((line) => {
+    const parts = line.split(',');
+    if (parts.length === 4) {
+      const firstname = parts[0].trim();
+      const field = parts[3].trim();
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    }
+  });
+
+  const totalStudents = students.length;
+
+  console.log(`Number of students: ${totalStudents}`);
+
+  for (const [field, firstnames] of Object.entries(fields)) {
+    console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
+  }
 }
 
 module.exports = countStudents;
